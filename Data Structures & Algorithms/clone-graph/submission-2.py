b9a84150ee46d2bca647fe846e5f4f61700cb689
newtonma7 <123204560+newtonma7@users.noreply.github.com>
2026-09-02 23:08:24 -0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,53 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        '''
+        u: need to create exact mapping and deep copy of graph 
+            can use hashmap to map old to new
+            dfs each node in the neighbors list to create mapping/find edges
+
+        p:
+            dfs,
+                base case:
+                    the node has no neighbors, return
+
+                iterative step:
+                    iterate neighbors, dfs each one
+
+                pass in current node,
+                make the current node with hashmap mapping,
+                iterate over list of neighbors and dfs into each one
+
+                can just use old node as the key to the hashmap to new node
+
+            add all values of hashmap to the ans
+        '''
+        oldtonew = {}
+
+        if not node:
+            return
+
+        def dfs(curr):
+            if not curr.neighbors:
+                return Node(curr.val)
+            if curr in oldtonew:
+                return oldtonew[curr]
+
+            copy = Node(curr.val)
+            oldtonew[curr] = copy
+            
+            for n in curr.neighbors:
+                oldtonew[curr].neighbors.append(dfs(n))
+                    
+            return copy
+
+        return dfs(node)
+
+        
