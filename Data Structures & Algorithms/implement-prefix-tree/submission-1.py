07aa0a7e621c8566a0ac75e2cbb0680538c35c62
newtonma7 <123204560+newtonma7@users.noreply.github.com>
2026-09-17 22:49:18 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,32 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
+        curr['.'] = '.'
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return '.' in curr
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return True
+        
